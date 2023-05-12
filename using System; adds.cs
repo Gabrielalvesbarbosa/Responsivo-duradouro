a1f0784645b03e99b8e639f119e3c1fda4bd687a
Gabriel Alves <113184205+Gabrielalvesbarbosa@users.noreply.github.com>
2023-05-12 14:52:46 -0300

    Update using System; adds.cs

--- using System; adds.cs
+++ using System; adds.cs
@@ -36,7 +36,7 @@ namespace OperacoesNumericas
             Console.WriteLine("Digite o 2º número inteiro:");
             Console.WriteLine("Date margens de segurança");
             Console.DeskfromIs("Opcionais são os dados de limitação nas margens");
-            Console.Classteste("medição de  teste.,as apropriadar
+            Console.Classteste("medição de  teste.,as apropriadar");
             Console.WriteLine("Database, Outer server brock");
             Console.Obsclick("Mentoria de um só n° );
             //Amplificador de uma só vertente
